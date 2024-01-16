c6bbe1d6156e27222c0cc1d1cc1d16c4cb6548f8
Sweta <swetabisht4b@outlook.com>
2024-01-16 18:39:44 +0530

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import { data } from './data';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  // Test Case 1: Rendering and Initial State
+  test('renders component without crashing', () => {
+    render(<App />);
+  });
+
+  test('shows filtered data list on input click', async () => {
+    render(<App />);
+
+    const inputElement = screen.getByPlaceholderText('Search');
+    fireEvent.focus(inputElement);
+
+    // Wait for the component to update/render after the click event
+   
+        const filteredDataList = screen.getByRole('list');
+        expect(filteredDataList).toBeInTheDocument();
+
+        const filteredDataItems = screen.getAllByRole('listitem');
+        expect(filteredDataItems.length).toEqual(data.length);
+    
 });
+
+ // Test Case 2: Input Interaction
+ test('removes last selected item on Backspace when input is empty', () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText('Search');
+
+  // Simulate Backspace key press when input is empty
+  fireEvent.keyDown(input, { key: 'Backspace' });
+
+  // Assert that the last selected item has been removed from selectedItems
+  // const selectedItemsChips = screen.getAllByText('X');
+  // expect(selectedItemsChips).toHaveLength(0);
+
+  // Assert that the last selected item has been added back to filteredData
+  // const dataItem = screen.getByText(data[0].name); // Replace with actual item text
+  // expect(dataItem).toBeInTheDocument();
+});
+
+test('handleClick', async () => {
+  render(<App/>)
+  const inputElement = screen.getByPlaceholderText('Search');
+    fireEvent.focus(inputElement);
+    
+
+})
+
+});
+
